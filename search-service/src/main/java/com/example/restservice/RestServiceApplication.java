41dbd13107770ccaf97e32f0082abeb8a0710ac4
silkeunrau <silke.unrau@yahoo.de>
2021-11-17 20:02:47 +0100

    get product via rest-api from product-service

---
+++ search-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(RestServiceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Product quote = restTemplate.getForObject(
+                    "http://localhost:8081/product-service/api/product/1", Product.class);
+            log.info(quote.toString());
+        };
+    }
+}
