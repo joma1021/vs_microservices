56b4b7c8cbfb365142620ea88836c586aee1d2ef
silkeunrau <silke.unrau@yahoo.de>
2021-11-17 21:27:27 +0100

    get products via rest-api from product-service and give them back in a rest api. todo: filter array with search term

---
+++ search-service/src/main/java/com/example/restservice/SearchController.java
@@ -0,0 +1,38 @@
+package com.example.restservice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SearchController {
+
+	Logger logger = LoggerFactory.getLogger(SearchController.class);
+
+	@GetMapping("/search")
+	public Object[] products(@RequestParam(defaultValue = "") String term) {
+		Product[] products = getProducts();
+		Object[] result = Arrays.stream(products).filter(product -> product.getName().contains(term)).toArray();
+
+		return result;
+	}
+
+	private static Product[] getProducts()
+	{
+		final String uri = "http://localhost:8081/product-service/api/product/all";
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		Product[] products = restTemplate.getForObject(
+				uri, Product[].class);
+		return products;
+	}
+}
