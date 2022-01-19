5ae1977bf76231096a454f26e0c255bd612fa813
joma1021 <mariusjost@outlook.de>
2022-01-19 13:16:17 +0100

    basic tests added

---
+++ category-service/src/test/java/de/hska/webshop/categoryservice/CategoryServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package de.hska.webshop.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class CategoryServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ product-service/src/test/java/de/hska/wabshop/productservice/ProductServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package de.hska.wabshop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class ProductServiceApplicationTest {    
+    @Test
+    void contextLoads() {
+    }
+}
